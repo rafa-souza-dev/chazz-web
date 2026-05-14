e07196e10a7f6d233a04b3e8812fef5aed94fbd8
Rafael Souza <rafael.jose@ferreri.co>
2026-05-14 15:47:03 -0300

    update env var

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from "axios";
 
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:3000";
+  process.env.NEXT_PUBLIC_CHAZZ_API_URL ?? "http://localhost:3000";
 
 export const apiPublic = axios.create({ baseURL: API_BASE_URL });
 
